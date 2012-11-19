650a002461ab591334fe022ccd229023f76cfe7a
Abrackadabra <Abrackadabraa@gmail.com>
2012-11-20 01:56:22 +0400

    hot

--- src/misc/parallelsort/MasterSorter.java
+++ src/misc/parallelsort/MasterSorter.java
@@ -25,7 +25,7 @@ public class MasterSorter {
                 while (!executorService.awaitTermination(100, TimeUnit.MILLISECONDS)) {
                 }
             } catch (InterruptedException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                e.printStackTrace();
             }
             if (i >= strings.size()) {
                 break;
